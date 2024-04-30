4969c3dfdb1105eff181db931758bb0c6c3e4294
JasonEJohnson <johnson@take-e-way.de>
2024-04-30 17:23:37 +0200

    ---

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,4 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 --              AstroNvim Configuration Table
 -- All configuration changes should go inside of the table below
 
